73a365f9ab8fb422066f33963640a52b65fa95d6
yann cheyrezy <yann.cheyrezy@le-campus-numerique.fr>
2023-04-25 12:09:58 +0200

    map campus
    Veuillez saisir le message de validation pour vos modifications. Les lignes

---
+++ logo_campusnum_fond-blanc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="logo_campusnum_fond-blanc" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="tilesets/logo_campusnum_fond-blanc.jpg" width="100" height="100"/>
+</tileset>
